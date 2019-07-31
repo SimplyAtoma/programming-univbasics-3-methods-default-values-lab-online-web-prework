42fbe8fda59b14ba69501f891aadcdafab7649a5
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-07-31 20:58:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 
-def #meal_choice(veg1, veg2, protein="meat") 
-  puts "A plate of #{meat} with #{veg1} and #{veg2}."
+def #meal_choice(veg1, veg2, protein*="meat") 
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
   
